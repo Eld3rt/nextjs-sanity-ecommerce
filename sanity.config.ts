658c7682df4de9b3dedc848fefec5cfd92406ea6
Eld3rt <kirrey8ynwa@gmail.com>
2026-01-06 17:12:02 +0300

    some config fixes

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,14 @@
-"use client";
-import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
-import { apiVersion, dataset, projectId } from "@/shared/config/sanity";
-import { schema } from "./sanity/schemaTypes";
-import { structure } from "./sanity/structure";
-import { presentationTool } from "sanity/presentation";
+'use client'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { apiVersion, dataset, projectId } from '@/shared/config/sanity'
+import { schema } from './sanity/schemaTypes'
+import { structure } from './sanity/structure'
+import { presentationTool } from 'sanity/presentation'
 
 export default defineConfig({
-  basePath: "/admin/studio",
+  basePath: '/admin/studio',
   projectId,
   dataset,
   schema,
@@ -17,11 +17,11 @@ export default defineConfig({
     visionTool({ defaultApiVersion: apiVersion }),
     presentationTool({
       previewUrl: {
-        initial: "http://localhost:3000",
+        initial: process.env.NEXT_PUBLIC_BASE_URL,
         previewMode: {
-          enable: "/api/draft-mode/enable",
+          enable: '/api/draft-mode/enable',
         },
       },
     }),
   ],
-});
+})
